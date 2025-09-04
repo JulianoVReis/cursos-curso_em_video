4ce138bb4d1f5e2f70c8feb9f8f41309ec430cbf
Juliano Reis <julianovreis@gmail.com>
2025-09-04 14:38:52 -0300

    update mysql

---
+++ mysql/aula-19.sql
@@ -0,0 +1,9 @@
+select nome, cursopreferido from gafanhotos;
+
+select nome, ano from cursos;
+
+select * from gafanhotos;
+
+select g.nome, c.nome, c.ano from gafanhotos as g right outer join cursos as c
+on c.idcurso = g.cursopreferido
+order by g.nome;
